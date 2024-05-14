d1bbd06a2157217916fe06154871640c4c9e33ad
Miyuqi12 <154991136+Miyuqi12@users.noreply.github.com>
2024-05-14 18:07:45 +0700

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,65 +1,50 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Alert, Button, Image } from 'react-native';
+import { useState } from "react";
+import {Alert, Button, Text, TextInput, View} from "react-native";
 
-const App = () => {
-  const [text, setText] = useState('');
-
-  const checkPalindrome = () => {
-    const reversedText = text.split('').reverse().join('');
-    const isPalindrome = text === reversedText;
-
-    const alertMessage = isPalindrome
-      ? `Teks "${text}" adalah palindrome!`
-      : `Teks "${text}" bukan palindrome.`;
-
-    Alert.alert('Hasil Validasi', alertMessage);
+export default function App() {
+  const [inputData, setInputData] = useState('');
+  const [number, setNumber] = useState(0);
+  const sendButton = () => {
+    Alert.alert(inputData);
   };
-
+  const tambah = ()=> {
+    setNumber(number +1);
+  }
+  const kurang = ()=> {
+    setNumber(number -1);
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>ようこそ</Text>
-      <Text style={styles.title}>Palindrome Validator</Text>
-
-      <Image
-        source={{ uri: 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/genshin-impact-mona/sticker_2.png?d7379bb25870f2a87153b14d36039c98' }}
-        style={styles.image}
-      />
-
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        padding: 20,
+      }}
+    >
+      <Text>Sek Sinau UseState</Text>
       <TextInput
-        style={styles.textInput}
-        placeholder="テキスト、どこ?"
-        value={text}
-        onChangeText={(newText) => setText(newText)}
+        placeholder="Masukkan Namae"
+        value={inputData}
+        onChangeText={setInputData}
       />
+      <Button title="kliken wes" onPress={sendButton}/>
+      <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
 
-      <Button title="Validasi/検証" onPress={checkPalindrome} />
+      </View>
+        <Text>{number}</Text>
+      <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 50,
+      }}>
+        <Button title="+" onPress={tambah}/>
+        <Button title="-" onPress={kurang}/>
+      </View>
     </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  textInput: {
-    width: '100%',
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    marginBottom: 20,
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
-});
-
-export default App;
+  )
+}
